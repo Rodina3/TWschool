81351deed76953309637f16d73b75f2ec527f92a
Zhou Rong <buaazhourong@163.com>
2017-07-18 14:21:00 +0800

    complete section1/practice3

--- workspace/gpst-js-basic-collection-practice/main/section-1/practice-3.js
+++ workspace/gpst-js-basic-collection-practice/main/section-1/practice-3.js
@@ -5,7 +5,7 @@ module.exports = function collectSameElements(collectionA, objectB) {
 
   for(var i=0; i<collectionA.length;i++)
   {
-    for(var j=0;j<objectB.value[j].length;j++)
+    for(var j=0;j<objectB.value.length;j++)
     {
       if(collectionA[i] == objectB.value[j])
       {
--- workspace/gpst-js-basic-collection-practice/main/section-1/practice-4.js
+++ workspace/gpst-js-basic-collection-practice/main/section-1/practice-4.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var same_elements = [];
+  for(var i=0;i<collectionA.length;i++)
+  {
+    for(var j=0;j<objectB.value.length;j++)
+    {
+      if(collectionA[i].key == objectB.value[j])
+      {
+        same_elements.push(collectionA[i].key);
+        break;
+      }
+    }
+  }
+  return same_elements;
 }
