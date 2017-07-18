37d838cc09160eea4b931f7a3d355b770718ab60
Zhou Rong <buaazhourong@163.com>
2017-07-18 23:25:21 +0800

    complete section3/practice3

--- workspace/gpst-js-basic-collection-practice/main/section-3/practice-3.js
+++ workspace/gpst-js-basic-collection-practice/main/section-3/practice-3.js
@@ -1,5 +1,71 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+
+    let collectionC = [];
+
+    collectionC = countSameEle(collectionA);
+
+    console.log("--debug3-3:1------------------")
+    console.log(collectionC);
+
+    for(let i=0;i<collectionC.length;i++)
+    {
+        for(let j=0;j<objectB.value.length;j++)
+        {
+            if(collectionC[i].key == objectB.value[j])
+            {
+                collectionC[i].count = process3sub1(collectionC[i].count);
+                break;
+            }
+        }
+    }
+
+    return collectionC;
+}
+
+function countSameEle(collection)
+{
+  let same_collection = [];var index=0;
+
+  for(let i =0;i<collection.length;i++)
+  {
+    index = Find(collection[i],same_collection);
+    if(index == -1)
+    {
+      same_collection.push({key:collection[i],count:1});
+    }
+    else
+    {
+      same_collection[index].count++;
+    }
+  }
+
+  return same_collection;
+
+}
+
+function Find(ele,collection)
+{
+  for(let i=0;i<collection.length;i++) {
+      if (ele == collection[i].key)
+          return i;
+  }
+
+    return -1;
+}
+
+
+
+function process3sub1(num)
+{
+    var loop_count = num;
+    for(let i=1;i <= loop_count;i++)
+    {
+        if(i%3==0)
+        {
+            num--;
+        }
+    }
+    return num;
 }
