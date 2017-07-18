2508d7c907578a63818bb14a27906d07f5f69234
Zhou Rong <buaazhourong@163.com>
2017-07-18 16:35:13 +0800

    complete section3/practice2

--- workspace/gpst-js-basic-collection-practice/main/section-3/practice-2.js
+++ workspace/gpst-js-basic-collection-practice/main/section-3/practice-2.js
@@ -1,5 +1,32 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+
+    for(let i=0;i<collectionA.length;i++)
+    {
+        for(let j=0;j<objectB.value.length;j++)
+        {
+            if(collectionA[i].key == objectB.value[j])
+            {
+                collectionA[i].count = process(collectionA[i].count);
+                break;
+            }
+        }
+    }
+    console.log("3.1----------------")
+    console.log(collectionA);
+    return collectionA;
+}
+
+function process(num)
+{
+  var loop_count = num;
+  for(let i=1;i <= loop_count;i++)
+  {
+    if(i%3==0)
+    {
+      num--;
+    }
+  }
+  return num;
 }
