329394ab1af1f488e85f7a38765b595d7eb2a84f
Zhou Rong <buaazhourong@163.com>
2017-07-19 00:04:14 +0800

    complete section3/practice4

--- workspace/gpst-js-basic-collection-practice/main/section-3/practice-4.js
+++ workspace/gpst-js-basic-collection-practice/main/section-3/practice-4.js
@@ -1,5 +1,114 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+
+  let collectionC = [];
+
+  collectionC = countSameEle(collectionA);
+  console.log('----3-4:1------');
+  console.log(collectionC);
+
+  collectionC = subSameEle(collectionC,objectB);
+  console.log('----3-4:2------');
+  console.log(collectionC);
+
+  return collectionC;
 }
+
+
+function countSameEle(collection)
+{
+    let same_collection = [];
+   // let index = 0;
+
+    for(let i =0;i<collection.length;i++)
+    {
+        /*index = Find(collection[i],same_collection);
+        if(index == -1)
+        {
+            same_collection.push({key:collection[i],count:1});
+        }
+        else
+        {
+            same_collection[index].count++;
+        }*/
+
+        if(collection[i].length === 1)
+          countSingleEle(collection[i],same_collection);
+        else
+          countLongEle(collection[i],same_collection);
+    }
+
+    return same_collection;
+
+}
+
+function countSingleEle(ele,same_arr)
+{
+  let index;
+  index = Find(ele,same_arr);
+  if(index === -1)
+  {
+      same_arr.push({key:ele,count:1});
+  }
+  else
+  {
+      same_arr[index].count++;
+  }
+  return same_arr;
+
+}
+
+function countLongEle(ele,same_arr)
+{
+  let letter = ele.split('-')[0];
+  let number = parseInt(ele.split('-')[1]);
+  let index = Find(letter,same_arr);
+    if (index !== -1) {
+        same_arr[index].count += number;
+    }
+    else {
+        same_arr.push({key:letter, count: number});
+    }
+  return same_arr;
+}
+
+function Find(ele,collection)
+{
+    for(let i=0;i<collection.length;i++) {
+        if (ele == collection[i].key)
+            return i;
+    }
+
+    return -1;
+}
+
+function subSameEle(arr1,arr2)
+{
+    for(let i=0;i<arr1.length;i++)
+    {
+        for(let j=0;j<arr2.value.length;j++)
+        {
+            if(arr1[i].key === arr2.value[j])
+            {
+                arr1[i].count = process3sub1(arr1[i].count);
+                break;
+            }
+        }
+    }
+    return arr1;
+}
+
+function process3sub1(num)
+{
+    var loop_count = num;
+    for(let i=1;i <= loop_count;i++)
+    {
+        if(i%3==0)
+        {
+            num--;
+        }
+    }
+    return num;
+}
+
