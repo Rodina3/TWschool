12de3dbdd9af86a5072f973f4896695a6fbc9f67
Zhou Rong <buaazhourong@163.com>
2017-07-20 15:37:30 +0800

    new branch-dev

--- workspace/pos/pos_v1/main/main.js
+++ workspace/pos/pos_v1/main/main.js
@@ -1,3 +1,60 @@
 'use strict';
 
 //TODO: 请在该文件中实现练习要求并删除此注释
+
+const allItemInfo = loadAllItems();
+const allPromtionItem = loadPromotions();
+
+function printReceipt(tags) {
+
+    let receipt = buildCart(tags);
+    let receiptText = buildReceiptText(receipt);
+    console.log(receiptText);
+}
+
+function buildCart(tags) {
+    let formatedTags = formatTag(tags);
+    let cartItem = countCartItem(formatedTags);
+    let receiptItem = calculateItemPrice(cartItem);
+    let receiptWithPromotion = promote(receiptItem);
+    let receipt = calculateReceipt(receiptItem);
+
+    return receipt;
+
+}
+
+function buildReceiptText(obj) {
+
+}
+
+function formatTag(tags)
+{
+    for(let i=0;i<tags.length;i++)
+    {
+        if (tags[i].length === 10)
+        {
+            tags[i]= tags[i]+'-1';
+        }
+    }
+    return formatedTags;
+}
+
+function countCartItem(obj)
+{
+    return cartItem;
+}
+
+function calculateItemPrice()
+{
+    return receiptItem;
+}
+
+function promote()
+{
+    return receiptWithPromotion;
+}
+
+function calculateReceipt()
+{
+    return receipt;
+}
