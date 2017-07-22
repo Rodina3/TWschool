a89e7c0995bf4ac6a117e2b8526e79f32b6bf9ff
Zhou Rong <buaazhourong@163.com>
2017-07-22 21:33:45 +0800

    finish pratice-5 预估：20min，实际：13min19s

---
+++ workspace/oop-dojo-master/main/practice_5.js
@@ -0,0 +1,33 @@
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+
+    basic_introduce() {
+        return `My name is ${this._name}. I am ${this._age} years old.`;
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, myclass) {
+        super(name, age);
+        this._class = myclass;
+    }
+
+    introduce() {
+        return super.basic_introduce() + ` I am a Student. I am at Class ${this._class}.`;
+    }
+}
+
+class Worker extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    introduce() {
+        return super.basic_introduce() + ' I am a Worker. I have a job.';
+    }
+}
+
+export {Person, Student, Worker}
--- workspace/oop-dojo-master/spec/practice_5/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_5/main_spec.js
@@ -1,7 +1,15 @@
 "use strict";
 
-describe("OO入门", function(){
-    it("覆盖同时复用父类的方法", function(){
+import {Person,Student,Worker} from '../../main/practice_5'
 
+describe("OO入门", function () {
+    it("覆盖同时复用父类的方法", function () {
+        const p = new Person('Tom',21);
+        const s = new Student('Tom',21,2);
+        const w = new Worker('Tom',21,2);
+
+        expect(p.basic_introduce()).toEqual('My name is Tom. I am 21 years old.');
+        expect(s.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Student. I am at Class 2.');
+        expect(w.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Worker. I have a job.');
     });
 });
