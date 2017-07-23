a8e5caf25b4ec0aca51d5540ef7868b5d1caab8d
Zhou Rong <buaazhourong@163.com>
2017-07-23 12:42:55 +0800

    finish practice-10 预估：40min 实际：28min40s

--- workspace/oop-dojo-master/spec/practice_10/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_10/main_spec.js
@@ -1,7 +1,20 @@
 "use strict";
 
-describe("OO入门", function(){
-    it("对象之间的交互-4", function(){
+import {Class, Student} from '../../main/practice_10'
+
+describe("OO入门", function () {
+    it("对象之间的交互-4", function () {
+        const c2 = new Class(2);
+        const c3 = new Class(3);
+
+        const sTom = new Student(100, 'Tom',21, c2);
+        const sBob = new Student(101, 'Bob',21, c2);
+        c2.assignLeader(sTom);
+        expect(sTom.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.');
+        expect(sBob.introduce()).toEqual('My name is Bob. I am 21 years old. I am a Student. I am at Class 2.');
+
+        const sJerry = new Student(102, 'Jerry', 21,c3);
+        expect(c2.assignLeader(sJerry)).toEqual('It is not one of us.');
 
     });
 });
