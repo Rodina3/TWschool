64f295e1f48e6135d440a342d8b4a55d08e9aeb7
Zhou Rong <buaazhourong@163.com>
2017-07-24 16:17:34 +0800

    finish java practice-3 预估：20min 实际：25min31s

---
+++ workspace/oop-dojo-java/src/main/java/practice_3/Student.java
@@ -0,0 +1,18 @@
+package practice_3;
+
+/**
+ * Created by rzhou on 24/07/2017.
+ */
+public class Student extends Person {
+
+    private int myclass;
+
+    public Student(String name, int age, int myclass) {
+        super(name, age);
+        this.myclass = myclass;
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + this.myclass + ".";
+    }
+}
---
+++ workspace/oop-dojo-java/src/test/java/practice_3/Practice3Test.java
@@ -0,0 +1,23 @@
+package practice_3;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by rzhou on 24/07/2017.
+ */
+public class Practice3Test {
+    @Test
+    public void testPersonIntroduce() throws Exception {
+        Person person = new Person("Tom", 21);
+        assertThat(person.introduce(), is("My name is Tom. I am 21 years old."));
+    }
+
+    @Test
+    public void testStudentIntroduce() throws Exception {
+        Student student = new Student("Tom", 21, 2);
+        assertThat(student.introduce(), is("My name is Tom. I am 21 years old. I am a Student. I am at Class 2."));
+    }
+}
