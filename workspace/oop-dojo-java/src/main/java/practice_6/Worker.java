de3af64776eeb82957afecac96397114439ffeef
Zhou Rong <buaazhourong@163.com>
2017-07-24 17:15:16 +0800

    finish java practice-6 预估：5min 实际：3min35s

---
+++ workspace/oop-dojo-java/src/main/java/practice_6/Worker.java
@@ -0,0 +1,14 @@
+package practice_6;
+
+/**
+ * Created by rzhou on 24/07/2017.
+ */
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Worker. I have a job.";
+    }
+}
