ef254eb48a3e18d3666dab8aebeb575d4f3186e4
Zhou Rong <buaazhourong@163.com>
2017-08-04 01:24:28 +0800

    [ZR]finished html template showing

--- workspace/zhourong-school-manager/js/AddPage.js
+++ workspace/zhourong-school-manager/js/AddPage.js
@@ -9,21 +9,21 @@ $(document).ready(function () {
             klass: "required",
             phone: {
                 required: true,
-                minlength: 11
+                minlength: 11,
             },
             email: {
                 required: true,
-                email: true
-            }
+                email: true,
+            },
         },
         messages: {
             name: "请输入学生名字",
             klass: "请输入学生班级",
             phone: {
                 required: "请输入学生电话",
-                minlength: "电话号码非法，必须为11位"
+                minlength: "电话号码非法，必须为11位",
             },
-            email: "请输入学生邮箱"
+            email: "请输入学生邮箱",
         },
 
         errorElement: "em",
@@ -36,32 +36,46 @@ $(document).ready(function () {
             } else {
                 error.insertAfter(element);
             }
-        },
-        highlight: function (element, errorClass, validClass) {
-            $(element).parents(".col-sm-5").addClass("has-error").removeClass("has-success");
-        },
-        unhighlight: function (element, errorClass, validClass) {
-            $(element).parents(".col-sm-5").addClass("has-success").removeClass("has-error");
         }
+        // },
+        // highlight: function (element, errorClass, validClass) {
+        //     $(element).parents(".col-sm-5").addClass("has-error").removeClass("has-success");
+        // },
+        // unhighlight: function (element, errorClass, validClass) {
+        //     $(element).parents(".col-sm-5").addClass("has-success").removeClass("has-error");
+        // }
     });
 
 
 });
 
-function saveStudentInfo() {
-    var obj = $('#student-info').serializeArray();
-    var str = JSON.stringify(obj);
-    localStorage.setItem('studentInfo', str);
-
-}
-
-
 $.validator.setDefaults({
     submitHandler: function () {
+        //添加不成功的时候，去掉这句话
         saveStudentInfo();
         $('#add-success-notice').text("添加学生成功");
         //刷新表单
-        //添加不成功的时候，去掉这句话
+
     }
 });
 
+var count=0;
+function saveStudentInfo() {
+    var formObj = $('#student-info').serializeArray();
+    //判断是否重复添加
+    //function
+    var student = formatStudent(formObj);
+    var str = JSON.stringify(student);
+    localStorage.setItem('studentInfo'+count, str);
+    count++;
+}
+
+
+function formatStudent(obj) {
+    var student = {};
+    for (let i = 0; i < obj.length; i++) {
+        student[obj[i].name] = obj[i].value;
+    }
+    student.id=count;
+    return student;
+}
--- workspace/zhourong-school-manager/js/SearchPage.js
+++ workspace/zhourong-school-manager/js/SearchPage.js
@@ -3,12 +3,40 @@
  */
 $(document).ready(function () {
     $('#search-button').click(function () {
-        var stuStr = localStorage.getItem('studentInfo');
-        var studentInfo = JSON.parse(stuStr);
-        for(var i=0;i<studentInfo.length;i++)
-        {
-            console.log("key:"+studentInfo[i].name+"value:"+studentInfo[i].value);
+        for (let i = 0; i < localStorage.length; i++) {
+            var stuStr = localStorage.getItem('studentInfo' + i);
+            var student = JSON.parse(stuStr);
+            //showReportHeader();
+            showStudentItem(student);
         }
-
     });
 });
+
+
+function showStudentItem(obj) {
+    //$('#student-inquire').html("");
+    var markup = "<tr> " +
+        "<td>${name}</td> " +
+        "<td>${id}</td> " +
+        "<td>${klass}</td> " +
+        "<td>${email}</td> " +
+        "<td>${phone}</td>" +
+        "</tr>";
+
+    /* Compile the markup as a named template */
+    $.template("studentTemplate", markup);
+    /* Render the template with the movies data and insert
+     the rendered HTML under the "movieList" element */
+    $.tmpl("studentTemplate", obj).appendTo("#student-inquire");
+}
+
+
+function onShow() {
+    for (let i = 0; i < localStorage.length; i++) {
+        var stuStr = localStorage.getItem('studentInfo' + i);
+        var student = JSON.parse(stuStr);
+        //showReportHeader();
+        showStudentItem(student);
+    }
+
+}
