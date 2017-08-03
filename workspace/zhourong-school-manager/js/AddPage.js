6756f3cd22d5e2276756c8d93f4e2c2d0cdac50c
Zhou Rong <buaazhourong@163.com>
2017-08-03 22:07:06 +0800

    [ZR]finished Add Page validation[expected:30min actual:1h4min]

--- workspace/zhourong-school-manager/js/AddPage.js
+++ workspace/zhourong-school-manager/js/AddPage.js
@@ -2,10 +2,50 @@
  * Created by rzhou on 03/08/2017.
  */
 $(document).ready(function () {
-    $('#add-submit').click(function () {
-        saveStudentInfo();
-        alert("添加学生成功！");
+
+    $("#student-info").validate({
+        rules: {
+            name: "required",
+            klass: "required",
+            phone: {
+                required: true,
+                minlength: 11
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: "请输入学生名字",
+            klass: "请输入学生班级",
+            phone: {
+                required: "请输入学生电话",
+                minlength: "电话号码非法，必须为11位"
+            },
+            email: "请输入学生邮箱"
+        },
+
+        errorElement: "em",
+        errorPlacement: function (error, element) {
+            // Add the `help-block` class to the error element
+            error.addClass("help-block");
+
+            if (element.prop("type") === "checkbox") {
+                error.insertAfter(element.parent("label"));
+            } else {
+                error.insertAfter(element);
+            }
+        },
+        highlight: function (element, errorClass, validClass) {
+            $(element).parents(".col-sm-5").addClass("has-error").removeClass("has-success");
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).parents(".col-sm-5").addClass("has-success").removeClass("has-error");
+        }
     });
+
+
 });
 
 function saveStudentInfo() {
@@ -14,3 +54,14 @@ function saveStudentInfo() {
     localStorage.setItem('studentInfo', str);
 
 }
+
+
+$.validator.setDefaults({
+    submitHandler: function () {
+        saveStudentInfo();
+        $('#add-success-notice').text("添加学生成功");
+        //刷新表单
+        //添加不成功的时候，去掉这句话
+    }
+});
+
