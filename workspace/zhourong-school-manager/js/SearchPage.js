24677527637ee59f13c5ce0cb95cea508a2495eb
Zhou Rong <buaazhourong@163.com>
2017-08-06 15:34:43 +0800

    [ZR]school manager web project:finished finding student by name in Search Page

--- workspace/zhourong-school-manager/js/SearchPage.js
+++ workspace/zhourong-school-manager/js/SearchPage.js
@@ -4,9 +4,9 @@
 $(document).ready(function () {
 
     $('#search-id-bn').click(function () {
-
+        $("#name-searched").val(" ");
         if (findStudentById() !== null) {
-            $('#search-notice').text("已找到。");
+            $('#search-notice').text("找到1项。");
             showReportHeadder();
             showStudentItem(findStudentById());
         }
@@ -16,19 +16,56 @@ $(document).ready(function () {
         }
     });
 
+    $('#search-name-bn').click(function () {
+        $('#id-searched').val(" ");
+        var studentList = [];
+        studentList = findStudentByName();
+        if (studentList.length !== 0) {
+
+            $('#search-notice').text("找到").append(studentList.length).append("项。");
+            // $('#search-notice').append(studentList.length);
+            // $('#search-notice').append("项。");
+
+            showReportHeadder();
+            for (let i = 0; i < studentList.length; i++) {
+                showStudentItem(studentList[i]);
+            }
+
+        }
+        else {
+            $('#search-notice').text("无结果，显示所有学生信息。");
+            onShow();
+        }
+    })
 
 });
 
+function findStudentByName() {
+    var studentList = [];
+    var inputName = $('#name-searched').val();
+    for (let i = 0; i < localStorage.length; i++) {
+        var stuStr = localStorage.getItem('studentInfo' + i);
+        if (stuStr !== null) {
+            var student = JSON.parse(stuStr);
+            if (student.name == inputName) {
+                studentList.push(student);
+            }
+        }
+        else
+            break;
+    }
+    return studentList;
+}
 
 
 function findStudentById() {
-    var formId = $('#id-searched').val();
+    var inputId = $('#id-searched').val();
 
     for (let i = 0; i < localStorage.length; i++) {
         var stuStr = localStorage.getItem('studentInfo' + i);
         if (stuStr !== null) {
             var student = JSON.parse(stuStr);
-            if (student.id == formId) {
+            if (student.id == inputId) {
                 return student;
             }
         }
